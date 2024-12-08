4fc646c7b9d59980c97b534883c69d752525f5e4
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-12-09 02:18:28 +0300

    test: написал проверку бенчмарков для складывания строк

---
+++ tests/test_concat_benchmark.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src.string_concat_benchmark import StringConcatBenchmark
+
+
+string_list_sizes = [10**3, 10**5, 10**6]
+
+
+@pytest.mark.parametrize("size", string_list_sizes)
+def test_string_concat_with_plus(benchmark, size):
+    strings = ["a" * 10] * size  # Генерируем список строк
+    benchmark(StringConcatBenchmark.concat_with_plus, strings)
+
+@pytest.mark.parametrize("size", string_list_sizes)
+def test_string_concat_with_join(benchmark, size):
+    strings = ["a" * 10] * size  # Генерируем список строк
+    benchmark(StringConcatBenchmark.concat_with_join, strings)
