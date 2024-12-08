fa185ccd75cd0a9b1dd364714f110af040822476
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-12-09 02:22:15 +0300

    feat: написал отрисовку красивых результатов

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import logging
 import platform
 
+from src import json_cleaner
+from src.json_to_table import json_to_table
+
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
@@ -8,6 +11,9 @@ logger = logging.getLogger(__name__)
 
 def main() -> None:
     logger.info(platform.python_version())
+
+    data = json_cleaner.clear_benchmarks()
+    print(json_to_table(data))
 
 
 if __name__ == "__main__":
